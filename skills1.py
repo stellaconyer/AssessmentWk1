d2d484157aad24efbfd5c0b892e11c8a790b5f91
Hackbright Student <stella@stellacotton.com>
2013-10-04 17:18:06 -0700

    Cleaning up functions

--- skills1.py
+++ skills1.py
@@ -2,10 +2,11 @@
 my_ints = [5, 4, 2, 25]
 my_list = ["one", "two", "four", "or", "more"] 
 
+
 # Write a function that takes a list and returns a new list with only the odd numbers.
 def all_odd(some_list):
     new_list = []    
-    for i in my_list:
+    for i in some_list:
         if i % 2 == 1:
             new_list.append(i)
     return new_list
@@ -13,7 +14,7 @@ def all_odd(some_list):
 # Write a function that takes a list and returns a new list with only the even numbers.
 def all_even(some_list):
     new_list = []
-    for i in my_list:
+    for i in some_list:
         if i % 2 == 0:
             new_list.append(i)
     return new_list
@@ -21,9 +22,9 @@ def all_even(some_list):
 # Write a function that takes a list of strings and a new list with all strings of length 4 or greater.
 
 
-new_list = []
 def long_words(word_list):
-    for i in my_list:
+    new_list = []
+    for i in word_list:
         if len(i) >= 4:    
             new_list.append(i)
     return new_list
@@ -33,51 +34,52 @@ def long_words(word_list):
 
 
 def smallest(some_list):
-    smalls = my_ints[0]
-    for i in my_ints:
+    smalls = some_list[0]
+    for i in some_list:
         if i < smalls:
             smalls =  i
     return smalls    
 
 
 def bubble_sort(some_list):
-    bubble_range = len(my_ints)-1  
+    bubble_range = len(some_list)-1  
     while bubble_range != 0: 
         for i in range(bubble_range):
-            print bubble_range, my_ints[i], my_ints[i + 1]  
-            if my_ints[i] > my_ints[i + 1]:
-                my_ints[i], my_ints[i+1] = my_ints[i+1], my_ints[i]
+            print bubble_range, some_list[i], some_list[i + 1]  
+            if some_list[i] > some_list[i + 1]:
+                some_list[i], some_list[i+1] = some_list[i+1], some_list[i]
         bubble_range = bubble_range - 1
              
-    return my_ints
+    return some_list
 
 # Write a function that finds the largest element in a list of integers and returns it.
 def largest(some_list):
-    large = my_ints[0]
-    for i in my_ints:
+    large = some_list[0]
+    for i in some_list:
         if i > large:
             large =  i
     return large   
 
-print largest(my_ints)
 
 # Write a function that takes a list of numbers and returns a new list of all those numbers divided by two.
 def halvesies(some_list):
-    for i in my_ints:
+    new_list = []
+    for i in some_list:
         new_list.append(i/2)
     return new_list
 
 
 # Write a function that takes a list of words and returns a list of all the lengths of those words.
 def word_lenths(word_list):
-    for i in my_list:
-        new_list.append(len(i))
+    new_list = []
+    for word in word_list:
+        new_list.append(len(word))
     return new_list
 
 # Write a function (using iteration) that sums all the numbers in a list.
 def sum_numbers(numbers):
     sum_of_nums = 0
-    for i in my_ints:
+    for i in numbers:
         sum_of_nums += i
     return sum_of_nums
 
@@ -86,7 +88,7 @@ def sum_numbers(numbers):
 # Write a function that multiplies all the numbers in a list together.
 def mult_numbers(numbers):
     mult_num = 1
-    for i in my_ints:
+    for i in numbers:
         mult_num = mult_num*i
     return mult_num
 
@@ -95,13 +97,13 @@ def mult_numbers(numbers):
 # Write a function that joins all the strings in a list together (without using the join method) and returns a single string.
 def join_strings(string_list):
     string = ""
-    for i in my_list:
+    for i in string_list:
         string = string+i
     return string
 
 # Write a function that takes a list of integers and returns the average (without using the avg method)
 def average(numbers):
     sum = 0
-    for i in my_ints:
+    for i in numbers:
         sum = sum + i
-    return sum/len(my_ints)
+    return sum/len(numbers)
